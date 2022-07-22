8f8a69f6c22b74b64daf3493a8c0db433581513f
AlgerPire <algerpire0@gmail.com>
2022-07-22 09:46:50 +0200

    "Working App "

--- firstGame/src/main/java/services/PlayerImplementation.java
+++ firstGame/src/main/java/services/PlayerImplementation.java
@@ -6,6 +6,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.Ingres9Dialect;
 
 import java.lang.reflect.Array;
 import java.sql.ClientInfoStatus;
@@ -41,37 +42,85 @@ public class PlayerImplementation {
             session.beginTransaction();
             List randomPlayer= session.createQuery("SELECT pname FROM player ORDER BY RAND()").list();
             String randomFinal= (String) randomPlayer.get(0);
-            System.out.println(randomFinal);
             int j=0;
 
-            while(true){
-                System.out.println("Give a player Name");
-                String name =scanner.nextLine();
-                if(name.equals(randomFinal)){
+            while(true) {
+                System.out.println("\n Give a player Name");
+                String name = scanner.nextLine();
+                if (name.equalsIgnoreCase(randomFinal)) {
                     System.out.println("Congrats, you found it");
-                    Query finalPlayer=session.createQuery("Select pname,team,league,position from player where pname=?");
-                    finalPlayer.setString(0,randomFinal);
+                    System.exit(0);
+                    break;
 
                 }
-                else{
-                    j=j+1;
-                    if(j==1){
-                        System.out.println("You havent found it. 4 Help Left");
-                        System.out.println("Here is a help. \n Team:");
-                            Query ql=session.createQuery("SELECT team FROM player where pname=?");
-                            ql.setString(0,randomFinal);
-                            List lista=ql.list();
-                            String team= (String) lista.get(0);
-                        System.out.println(team);
-                    }
-                    else if(j==2){
-                        System.out.println("no its not");
-                    }
-                    else if(j==3){
-                        System.out.println("Two left");
-                    }
-                }
+                else
+                {
+                    j = j + 1;
 
+                    switch (j) {
+                        case 1:{
+                            System.out.println("You havent found it.Here is HELP 1");
+                            Query query=session.createQuery("Select isactive from player where pname=?");
+                            query.setString(0, randomFinal);
+                            boolean isActive=(boolean) query.list().get(0);
+                            if(isActive){
+                                System.out.println("Player is active");
+                            }
+                            else {
+                                System.out.println("Player is not active");
+                            }
+                            break;
+
+                        }
+                        case 2:{
+                            System.out.println("You havent found it.Here is HELP 2");
+                            Query query = session.createQuery("SELECT nationality FROM player where pname=?");
+                            query.setString(0, randomFinal);
+                            String nationality = (String) query.list().get(0);
+                            System.out.println("Nationality: "+nationality);
+                            break;
+                        }
+                        case 3:{
+                            System.out.println("You havent found it.Here is HELP 3");
+                            Query query=session.createQuery("Select age from player where pname=?");
+                            query.setString(0, randomFinal);
+                            int age = (int) query.list().get(0);
+                            System.out.println("Age "+age);
+                            break;
+                        }
+                        case 4:{
+                            System.out.println("You havent found it.Here is HELP 4");
+                            Query query=session.createQuery("Select position from player where pname=?");
+                            query.setString(0, randomFinal);
+                            String position = (String) query.list().get(0);
+                            System.out.println("Position "+position);
+                            break;
+                        }
+                        case 5:{
+                            System.out.println("You havent found it.Here is HELP 5");
+                            Query query=session.createQuery("Select league from player where pname=?");
+                            query.setString(0, randomFinal);
+                            String league = (String) query.list().get(0);
+                            System.out.println("League: "+league);
+                            break;
+
+                        }
+                        case 6:{
+                            System.out.println("You havent found it.Here is LAST HELP");
+                            Query query=session.createQuery("Select team from player where pname=?");
+                            query.setString(0, randomFinal);
+                            String team = (String) query.list().get(0);
+                            System.out.println("Team: "+team);
+                            break;
+                        }
+                        case 7:{
+                            System.out.println("Sorry. You havent found it. The player was "+randomFinal);
+                            System.exit(0);
+                            break;
+                        }
+                    }
+
+                }
             }
 
 
@@ -84,5 +133,6 @@ public class PlayerImplementation {
             }
         }
     }
+
 
 }
